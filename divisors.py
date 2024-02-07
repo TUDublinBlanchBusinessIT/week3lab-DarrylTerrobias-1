20e46c7e9c27ed255d4eb04061a9e0e5f2e4b270
B00152361 Darryl Terrobias <B00152361@mytudublin.ie>
2024-02-07 12:16:13 +0000

    divisors.py running

--- divisors.py
+++ divisors.py
@@ -1,6 +1,6 @@
 #Darryl Terrobis
 #7 Feb 2024
-#Divisor.py
+#divisor.py
 
 #The red text are the steps to do
 
@@ -9,24 +9,26 @@
 #A divisor or factor is a number which divides evenly leaving no remainder
 
 #define the funciton header called divisors expecting one argument
-def divirors(num):
-    
+def divisors(num):
     #set up an empty list to hold your result
-    myList = []
-    
+    myList = []    
     #loop i from 1 to the number you are checking (take care not to include the number itself)
     for i in range(1, num):
-        if num % 1 == 0:
-            
-        #if the remainder when dividing the number by i is equal to zero
-        #then i is a divisor so...
+        if num % 1 == 0:            
+            #if the remainder when dividing the number by i is equal to zero
+            #then i is a divisor so...
+            myList.append(i)
+
+    return myList
+
+print(divisors(28))
         
     
             #apend i to the list you set up
 
-            myList.append(i)   
+          
  
     #return the list
-    return myList
 
-print(divirors(28))
+
+
--- perfectNumber.py
+++ perfectNumber.py
@@ -1,19 +1,23 @@
-#Add your name here
-#Add the date here
-#Add the filename here
+#Darryl Terrobias
+#7 Feb 2024
+#perfectNumber.py
 
 #A perfect number is one for which all the divisors of the number add up to the
 #number itself. For example the divisors of 28 are 1,2,4,7,14 which added together gives 28
 #write a function below called perfectNumber(x) which checks to see if x is a perfect number
 #The function should return True if the number is perfect and False if it is not
 
+
 from divisors import divisors
 #define the function header called perfectNumber expecting one argument
+def perfectNumber(num):
 
     #set a result variable to False by default
-    
+    result = False
     #if the sum of all the divisors of the number is equal to the test number
-
+    if sum_divisors ==x:
         #set the result variable to be True
-
+        result = True
     #return the result variable
+        return result
+
